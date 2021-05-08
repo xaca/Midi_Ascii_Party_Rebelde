fede818799ab0968dc028d8e92c13972dbcbd041
Andres Bedoya Tobon <andres.bedoya@upb.edu.co>
2021-05-07 21:26:15 -0500

    Actualizacion y avance funcionalidades

--- Programa.java
+++ Programa.java
@@ -114,29 +114,27 @@ public class Programa{
 	}
 
 	public static void menu(){
-		System.out.println("|                              _     _                |");
-		System.out.println("|                             ( \\---/ )                |");
-		System.out.println("|                              ) . . (                |");
-		System.out.println("|________________________,--._(___Y___)_,--._______________________ hjw                |");
-		System.out.println("|                        `--'           `--'                |");
-
-		System.out.println("|		                  _   _  __                       |");
-		System.out.println("|		                 | | (_)/ _|                      |");
-		System.out.println("|		  ___ _ __   ___ | |_ _| |_ _   _                 |");
-		System.out.println("|		 / __| '_ \\ / _ \\| __| |  _| | | |                |");
-		System.out.println("|		 \\__ \\ |_) | (_) | |_| | | | |_| |                |");
-		System.out.println("|		 |___/ .__/ \\___/ \\__|_|_|  \\__, |                |");
-		System.out.println("|		     | |                     __/ |                |");
-		System.out.println("|		     |_|                    |___/                 |");
-
-		System.out.println("| ");
-		System.out.println("|                    Ingrese una opción así:");
-		System.out.println("|                    1. Buscar canción");
-		System.out.println("|                    2. Reproducir canción");
-		System.out.println("|                    3. Mostrar Letra");
-		System.out.println("|                    4. Detener Canción");
-		System.out.println("|                    5. Imprimir lista de Canciones");
-		System.out.println("|                    6. Salir");
+		System.out.println("|                              _     _                             |");
+		System.out.println("|                             ( \\---/ )                            |");
+		System.out.println("|                              ) . . (                             |");
+		System.out.println("|________________________,--._(___Y___)_,--.____________________hjw|");
+		System.out.println("|                        `--'           `--'                       |");
+		System.out.println("|		                  _   _  __                        |");
+		System.out.println("|		                 | | (_)/ _|                       |");
+		System.out.println("|		  ___ _ __   ___ | |_ _| |_ _   _                  |");
+		System.out.println("|		 / __| '_ \\ / _ \\| __| |  _| | | |                 |");
+		System.out.println("|		 \\__ \\ |_) | (_) | |_| | | | |_| |                 |");
+		System.out.println("|		 |___/ .__/ \\___/ \\__|_|_|  \\__, |                 |");
+		System.out.println("|		     | |                     __/ |                 |");
+		System.out.println("|		     |_|                    |___/                  |");
+				  imprimir("|                                                          		|");
+				  imprimir("|                    Ingrese una opción así:		 	   |");
+				  imprimir("|                    1. Buscar canción			 	     |");
+				  imprimir("|                    2. Reproducir canción			     |");
+				  imprimir("|                    3. Mostrar Letra					 |");
+				  imprimir("|                    4. Detener Canción					 |");
+				  imprimir("|                    5. Imprimir lista de Canciones		   |");
+				  imprimir("|                    6. Salir							 |");
 	}
 
 	public static void main(String[] args) {
